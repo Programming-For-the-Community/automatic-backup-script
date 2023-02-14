aad0e8789408418862f96e7700ad063507fe91f6
ProfessorChaos0802 <1822rhit@pm.me>
2023-02-14 16:52:27 -0500

    Checks for Backups Folder on External Drive

--- AutomaticBackup.cs
+++ AutomaticBackup.cs
@@ -1,5 +1,24 @@
 ﻿class AutomaticBackup {
+    private string dDrive = @"D:\";
     static void Main(string[] args){
-        
+        AutomaticBackup ab = new AutomaticBackup();
+
+        bool hasDDrive = false;
+
+        try {
+            hasDDrive = Directory.Exists(ab.dDrive);
+        } catch (Exception e) {
+            Console.WriteLine(e.Message);
+        }
+
+        if(hasDDrive){
+            ab.checkForBackupsDir();
+        }
+    }
+
+    private void checkForBackupsDir(){
+        if(!Directory.Exists($"{dDrive}Backups")){
+            Directory.CreateDirectory($"{dDrive}Backups");
+        }
     }
 }
