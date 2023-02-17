8fb335bccc1dfbd20aadff609822d19a6c62d6eb
ProfessorChaos0802 <1822rhit@pm.me>
2023-02-17 18:48:49 -0500

    Trying a failed test

--- External Drive Backup/AutomaticBackupScript/AutomaticBackupTest/AutomaticBackupTest.cs
+++ External Drive Backup/AutomaticBackupScript/AutomaticBackupTest/AutomaticBackupTest.cs
@@ -7,9 +7,9 @@ public class UnitTest1
     [TestMethod]
     public void AutomaticBackupCreation()
     {
-        AutomaticBackup.Backup ab = new AutomaticBackup.Backup("C");
+        AutomaticBackup ab = new AutomaticBackup.Backup("C");
 
-        Assert.AreEqual(@"C:\", ab.DriveLocation);
+        Assert.AreEqual(@"X:\", ab.DriveLocation);
         Assert.AreEqual($@"{DateTime.Now.ToString("ddd-ddMMMyyyy")}\", ab.DailyFolder);
     }
 }
